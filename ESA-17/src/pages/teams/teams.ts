824afbd5e94f6738e867068e7e7f3d0474b6ee41
Elena Slavcheva <eslavcheva.edu@gmail.com>
2018-12-21 19:31:44 +0000

    Add ESA-17: Ionic Navigation

---
+++ ESA-17/src/pages/teams/teams.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TeamHomePage } from '../pages';
+
+@IonicPage()
+@Component({
+  selector: 'page-teams',
+  templateUrl: 'teams.html',
+})
+export class TeamsPage {
+  teams: any = [
+    {id: 1, name: 'HC Elite'},
+    {id: 2, name: 'Team Takeover'},
+    {id: 3, name: 'DC Thunder'}
+  ];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TeamsPage');
+  }
+
+  itemTapped($event, team) {
+    this.navCtrl.push(TeamHomePage, {team: team});
+}
+}
